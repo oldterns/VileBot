e46e7805d1179167f90a7042f62bc0c33867ae56
Emmett Underhill <eunderhi@redhat.com>
2016-07-25 16:54:33 -0400

    Adding trivia game

---
+++ vilebot/src/com/oldterns/vilebot/handlers/user/Trivia.java
@@ -0,0 +1,157 @@
+package com.oldterns.vilebot.handlers.user;
+
+import ca.szc.keratin.bot.annotation.HandlerContainer;
+import ca.szc.keratin.core.event.message.recieve.ReceivePrivmsg;
+import com.oldterns.vilebot.db.KarmaDB;
+import net.engio.mbassy.listener.Handler;
+import twitter4j.JSONArray;
+import twitter4j.JSONObject;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by eunderhi on 25/07/16.
+ */
+@HandlerContainer
+public class Trivia {
+
+    private static final Pattern questionPattern = Pattern.compile( "^!jeopardy" );
+    private static final Pattern answerPattern = Pattern.compile( "^!(whatis|whois) (.*)" );
+    private static TriviaGame currentGame = null;
+
+    @Handler
+    public void doTrivia(ReceivePrivmsg event) {
+        String text = event.getText();
+        Matcher questionMatcher = questionPattern.matcher(text);
+        Matcher answerMatcher = answerPattern.matcher(text);
+
+        try {
+            if (questionMatcher.matches()) {
+                startGame(event);
+            } else if (answerMatcher.matches()) {
+                String answer = answerMatcher.group(2);
+                finishGame(event, answer);
+            }
+        } catch(Exception e) {
+            event.reply("I don't feel like playing.");
+            e.printStackTrace();
+        }
+
+    }
+
+    private synchronized void startGame(ReceivePrivmsg event) throws Exception {
+        if (currentGame != null) {
+            event.reply(currentGame.getAlreadyPlayingString());
+        }
+        else {
+            currentGame = new TriviaGame();
+            event.reply(currentGame.getIntroString());
+        }
+    }
+
+    private synchronized void finishGame(ReceivePrivmsg event, String answer) {
+        if (currentGame != null) {
+            String answerer = event.getSender();
+            if (currentGame.sendAnswer(answer)) {
+                event.reply(String.format("Congrats %s, you win %d karma!", event.getSender(), currentGame.getStakes()));
+                KarmaDB.modNounKarma(answerer, currentGame.getStakes());
+            }
+            else {
+                event.reply(String.format("Sorry %s! We were looking for \"%s\", you lose %d karma.",
+                        event.getSender(), currentGame.getAnswer(), currentGame.getStakes()));
+                KarmaDB.modNounKarma(answerer, -1 * currentGame.getStakes());
+            }
+            currentGame = null;
+        }
+        else {
+            event.reply("No active game. Start a new one with !jeopardy");
+        }
+    }
+
+
+    private static class TriviaGame {
+
+        private int stakes;
+        private String question;
+        private String answer;
+        private String category;
+
+        private static final String API_URL = "http://jservice.io/api/random";
+
+        public TriviaGame() throws Exception {
+            String jsonContent = getQuestionJson();
+            JSONObject triviaJSON = new JSONArray(jsonContent).getJSONObject(0);
+            question = triviaJSON.getString("question");
+            category= triviaJSON.getJSONObject("category").getString("title");
+            answer = triviaJSON.getString("answer");
+            stakes = getRandomKarma();
+        }
+
+        private int getRandomKarma() {
+            return new Random().nextInt(9) + 1;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public int getStakes() {
+            return stakes;
+        }
+
+        public boolean sendAnswer(String answer) {
+            return isCorrect(answer);
+        }
+
+        private boolean isCorrect(String answer) {
+            String formattedCorrectAnswer = formatAnswer(this.answer);
+            String formattedUserAnswer = formatAnswer(answer);
+            return formattedCorrectAnswer.equals(formattedUserAnswer);
+        }
+
+        private String formatAnswer(String answer) {
+            return answer.toLowerCase()
+                    .replace("\\", "")
+                    .replaceAll("^the", "")
+                    .replaceAll("^a", "")
+                    .replace("\"", "");
+        }
+
+        private String getQuestionBlurb() {
+            return String.format(
+                    "Your category is: %s\nFor %d karma:\n%s",
+                    category, stakes, question);
+        }
+
+        public String getIntroString() {
+            return "Welcome to VileBot trivia!\n" + getQuestionBlurb();
+        }
+
+        public String getAlreadyPlayingString() {
+            return "A game is already in session!\n" + getQuestionBlurb();
+        }
+
+        private String getQuestionJson() throws Exception {
+            String content;
+            URLConnection connection;
+            connection = new URL(API_URL).openConnection();
+            connection.addRequestProperty(
+                    "User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
+            );
+            Scanner scanner = new Scanner(connection.getInputStream());
+            scanner.useDelimiter("\\Z");
+            content = scanner.next();
+            return content;
+        }
+    }
+
+}
