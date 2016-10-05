8dab9a69cf42ab153c4ed7fbf3e29d79e16719de
yuqi-zhang <jzehrarnyg@gmail.com>
2016-10-05 10:50:25 -0400

    Initial church commit
    Add functionality to donate, see donation total and see top donors.

---
+++ vilebot/src/com/oldterns/vilebot/db/ChurchDB.java
@@ -0,0 +1,193 @@
+package com.oldterns.vilebot.db;
+
+import java.util.Set;
+
+import redis.clients.jedis.Jedis;
+
+public class ChurchDB
+    extends RedisDB
+{
+	/**
+	 * This class is effectively a duplicate of KarmaDB, used for church related karma
+	 */
+
+	private static final String keyOfChurchDonorSortedSet = "church-donor-karma";
+	private static final String keyOfChurchSortedSet = "church-karma";
+
+    /**
+     * Change the karma of a noun by an integer.
+     *
+     * @param noun The noun to change the karma of
+     * @param mod The amount to change the karma by, may be negative.
+     */
+    public static void modDonorKarma( String noun, int mod )
+    {
+        Jedis jedis = pool.getResource();
+        try
+        {
+            jedis.zincrby( keyOfChurchDonorSortedSet, mod, noun );
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+    }
+    /**
+     * Change the karma not affiliated with a donor
+     *
+     */
+    public static void modNonDonorKarma( int mod )
+    {
+        Jedis jedis = pool.getResource();
+        try
+        {
+            jedis.zincrby( keyOfChurchSortedSet, mod, keyOfChurchSortedSet );
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+    }
+
+    /**
+     * Get the karma of a noun.
+     *
+     * @param noun The noun to query to karma of
+     * @return Integer iff the noun has a defined value, else null
+     */
+    public static Integer getDonorKarma( String noun )
+    {
+        Jedis jedis = pool.getResource();
+        Double karma;
+        try
+        {
+            karma = jedis.zscore( keyOfChurchDonorSortedSet, noun );
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+
+        if ( karma == null )
+        {
+            return null;
+        }
+
+        return Integer.valueOf( Long.valueOf( Math.round( karma ) ).intValue() );
+    }
+
+    /**
+     * Get the rank of a noun based on its karma.
+     *
+     * @param noun The noun to query the rank of
+     * @return Integer iff the noun has a defined value, else null
+     */
+    public static Integer getDonorRank( String noun )
+    {
+        Jedis jedis = pool.getResource();
+        Long rank;
+        try
+        {
+            rank = jedis.zrevrank( keyOfChurchDonorSortedSet, noun );
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+
+        if ( rank == null )
+        {
+            return null;
+        }
+
+        return Integer.valueOf( rank.intValue() + 1 );
+    }
+
+    /**
+     * Get nouns from karma ranks.
+     *
+     * @param lower The lower rank to get the nouns of.
+     * @param upper The upper rank to get the nouns of.
+     * @return String The noun iff the rank exists, else null.
+     */
+    public static Set<String> getDonorsByRanks( long lower, long upper )
+    {
+        Set<String> nouns;
+
+        Jedis jedis = pool.getResource();
+        try
+        {
+            nouns = jedis.zrevrange( keyOfChurchDonorSortedSet, lower, upper );
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+
+        if ( nouns == null || nouns.size() == 0 )
+        {
+            return null;
+        }
+
+        return nouns;
+    }
+
+    public static boolean removeDonor( String noun )
+    {
+        Long existed;
+
+        Jedis jedis = pool.getResource();
+        try
+        {
+            existed = jedis.zrem( keyOfChurchDonorSortedSet, noun );
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+
+        if ( existed == null || existed != 1 )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static long getTotalDonations() {
+        Jedis jedis = pool.getResource();
+        long totalKarma;
+        try
+        {
+            Set<String> members = jedis.zrange(keyOfChurchDonorSortedSet, 0, -1);
+            totalKarma = sum(members, jedis);
+        }
+        finally
+        {
+            pool.returnResource( jedis );
+        }
+        return totalKarma;
+    }
+
+    public static long getTotalNonDonations() {
+         Jedis jedis = pool.getResource();
+         long totalKarma;
+         try
+         {
+             Set<String> members = jedis.zrange(keyOfChurchSortedSet, 0, -1);
+             totalKarma = sum(members, jedis);
+         }
+         finally
+         {
+             pool.returnResource( jedis );
+         }
+         return totalKarma;
+    }
+
+    private static long sum(Set<String> members, Jedis jedis) {
+        long sum = 0;
+        for (String member : members) {
+            sum += jedis.zscore(keyOfChurchDonorSortedSet, member);
+        }
+        return sum;
+    }
+}
---
+++ vilebot/src/com/oldterns/vilebot/handlers/user/Church.java
@@ -0,0 +1,137 @@
+package com.oldterns.vilebot.handlers.user;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.oldterns.vilebot.db.KarmaDB;
+import com.oldterns.vilebot.db.ChurchDB;
+import com.oldterns.vilebot.util.BaseNick;
+
+import net.engio.mbassy.listener.Handler;
+import ca.szc.keratin.bot.annotation.HandlerContainer;
+import ca.szc.keratin.core.event.message.interfaces.Replyable;
+import ca.szc.keratin.core.event.message.recieve.ReceivePrivmsg;
+
+@HandlerContainer
+public class Church
+{
+    private static final Pattern donatePattern = Pattern.compile( "^!donate (-?[0-9]+)\\s*" );
+    private static final Pattern churchTotalPattern = Pattern.compile( "^!churchtotal" );
+    private static final Pattern topDonorsPattern = Pattern.compile( "^!topdonors" );
+
+    // churchKarmaEntryString will be stored in the database for all non-donated karma (tax, forfeiture)
+    private static final String churchKarmaEntryString = "Church of VileBot";
+
+    @Handler
+    private void donateToChurch( ReceivePrivmsg event )
+    {
+        Matcher matcher = donatePattern.matcher( event.getText() );
+
+        if ( matcher.matches() )
+        {
+            String rawDonationAmount = matcher.group( 1 );
+            String donor = BaseNick.toBaseNick( event.getSender() );
+
+            Integer donationAmount = Integer.parseInt( rawDonationAmount );
+            Integer donorKarma = KarmaDB.getNounKarma(donor);
+            donorKarma = donorKarma == null ? 0 : donorKarma;
+
+            if ( donationAmount <= 0)
+            {
+                event.reply("You cannot donate a non-positive number, imbecile.");
+            }
+            else if ( donorKarma <= 0 || donorKarma < donationAmount)
+            {
+                event.reply("You have insufficient karma to donate.");
+            }
+            else
+            {
+                ChurchDB.modDonorKarma( donor, donationAmount );
+                KarmaDB.modNounKarma( donor, -1 * donationAmount );
+
+                event.reply("Thank you for your donation " + donor + " of " + donationAmount
+                            + "! You are now rank " + ChurchDB.getDonorRank(donor) + " in donations.");
+            }
+        }
+    }
+
+    @Handler
+    private void viewChurchTotal( ReceivePrivmsg event )
+    {
+        Matcher matcher = churchTotalPattern.matcher( event.getText() );
+
+        if ( matcher.matches() )
+        {
+            long totalDonations = ChurchDB.getTotalDonations();
+            long churchTotal = totalDonations + ChurchDB.getTotalNonDonations();
+            event.reply("The church coffers contains " + churchTotal + ", of which " + totalDonations
+                        + " was contributed by its loyal believers.");
+        }
+    }
+
+    @Handler
+    private void viewTopDonors( ReceivePrivmsg event )
+    {
+        Matcher matcher = topDonorsPattern.matcher( event.getText() );
+        if ( matcher.matches() )
+        {
+            Set<String> nouns = null;
+
+            nouns = ChurchDB.getDonorsByRanks( 0, 6 );
+
+            if ( nouns != null && nouns.size() > 0 )
+            {
+                event.reply("NICK           AMOUNT    TITLE");
+                for ( String noun : nouns )
+                {
+                    replyWithRankAndDonationAmount( noun, event );
+                }
+            }
+            else
+            {
+                event.reply( "The church does not have enough followers yet." );
+            }
+        }
+    }
+
+    private static boolean replyWithRankAndDonationAmount( String noun, Replyable event )
+    {
+        Integer nounRank = ChurchDB.getDonorRank( noun );
+
+        Integer nounKarma = ChurchDB.getDonorKarma( noun );
+
+        if ( nounKarma != null )
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append( noun );
+            sb.reverse();
+
+            String nounString = sb.toString();
+            int nounLength = sb.length();
+            int spaceLength;
+            if ( nounLength <= 14 )
+            {
+                spaceLength = 15 - nounLength;
+            }
+            else
+            {
+                nounString = nounString.substring(0, 14);
+                spaceLength = 1;
+            }
+
+            String spaces_first = String.format("%" + spaceLength + "s", "");
+
+            if (nounKarma.toString().length() <= 9)
+            {
+                spaceLength = 10 - nounKarma.toString().length();
+            }
+
+            String spaces_second = String.format("%" + spaceLength + "s", "");
+            event.reply( nounString + spaces_first + nounKarma.toString() + spaces_second + "Cardinal");
+            return true;
+        }
+        return false;
+    }
+}
