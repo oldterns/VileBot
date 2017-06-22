b5d3e4c5f87594e12979bf6f2df81547beab0e31
Ian Pun <ipun@redhat.com>
2017-06-22 15:39:10 -0400

    Added logic for submissions in private

--- vilebot/src/main/java/com/oldterns/vilebot/handlers/user/Countdown.java
+++ vilebot/src/main/java/com/oldterns/vilebot/handlers/user/Countdown.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -18,6 +19,8 @@ import com.oldterns.vilebot.util.BaseNick;
 
 import bsh.EvalError;
 import bsh.Interpreter;
+import ca.szc.keratin.bot.KeratinBot;
+import ca.szc.keratin.bot.annotation.AssignedBot;
 import ca.szc.keratin.bot.annotation.HandlerContainer;
 import ca.szc.keratin.core.event.message.recieve.ReceivePrivmsg;
 import net.engio.mbassy.listener.Handler;
@@ -26,24 +29,29 @@ import net.engio.mbassy.listener.Handler;
 public class Countdown {
 
     private static final String COUNTDOWN_CHANNEL = Vilebot.getConfig().get("countdownChannel");
-	private static final long TIMEOUT  = 30000L;
+	private static final long TIMEOUT  = 45000L;
 	
 	
 	private static final Pattern countdownPattern = Pattern.compile("^!countdown");
 	private static final Pattern answerPattern = Pattern.compile("^!solution (.*)");
 	private static CountdownGame currGame = null;
     private static ExecutorService timer = Executors.newScheduledThreadPool(1);
+    
+    @AssignedBot
+    private KeratinBot bot;
 
 	private static class CountdownGame {
 		private final int VALID_NUMBER_COUNT = 6;
 		private final List <Integer> LARGE_NUMBERS = new ArrayList<>(Arrays.asList(25, 50, 75, 100));
 		private final List <Integer> SMALL_NUMBERS = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 		private List <Integer> questionNumbers = new ArrayList<>();
+		private Hashtable <String, String> submissions = new Hashtable<>(); 
 		private int largeNumberCount;
 		private int smallNumberCount;
 		private int targetNumber;
 		private int stakes;
 		Interpreter interpreter;
+		
 
 		Random rand = new Random();
 		
@@ -57,7 +65,6 @@ public class Countdown {
 			targetNumber = rand.nextInt(899) + 100;
 			// have karma stakes random from 1-10 for now. Not working yet.
 			stakes = rand.nextInt(11);
-			interpreter = new Interpreter();
 		}
 		
 		private int getLargeNumberCount() {
@@ -81,7 +88,7 @@ public class Countdown {
 		}
 		
 		private String getCountdownIntro() {
-			return "Welcome to Countdown!\n"+getQuestion()+"\n Good luck! You have 30 seconds.";
+			return "Welcome to Countdown!\n"+getQuestion()+"\n Good luck! You have 45 seconds.";
 		}
 		
 		private String getQuestion() {
@@ -93,24 +100,18 @@ public class Countdown {
 			Collections.shuffle(SMALL_NUMBERS);
 		}
 		
-		
-		private boolean isCorrect(String answer) {
+		private int interpretedAnswer(String answer) throws Exception {
 			if (noSpecialCharacters(answer) && hasCorrectNumbers(answer)) {
-				int result = interpretedAnswer(answer);
-				return ((targetNumber-10) <= result && result <= (targetNumber+10));
+				interpreter = new Interpreter();
+				try {
+					interpreter.eval("result = "+answer);
+					return ((int) interpreter.get("result"));
+				} catch (EvalError e) {
+					e.printStackTrace();
+					throw e;
+				}
 			} else {
-				return false;
-			}
-		}
-		
-		private int interpretedAnswer(String answer) {
-			interpreter = new Interpreter();
-			try {
-				interpreter.eval("result = "+answer);
-				return ((int) interpreter.get("result"));
-			} catch (EvalError e) {
-				e.printStackTrace();
-				return 0;
+				throw new ArithmeticException("invalid answer string");
 			}
 		}
 		
@@ -137,7 +138,7 @@ public class Countdown {
 		}
 		
 		private String alreadyPlaying() {
-			return "A current game is already in progress.\n"+getQuestionNumbers()+"\nYour target is \n"+getTargetNumber();
+			return "A current game is already in progress.\n"+getQuestion();
 		}
 	}
 	
@@ -148,16 +149,48 @@ public class Countdown {
 		Matcher answerMatcher= answerPattern.matcher(text);
 		if (countdownMatcher.matches() && correctChannel(event)) {
 			startCountdownGame(event);
-		} else if (answerMatcher.matches() && correctChannel(event)) {
+		} else if (answerMatcher.matches() && correctSolutionChannel(event)) {
 			String answer = answerMatcher.group(1);
-			event.reply("your solution is :"+answer);
 			checkAnswer(event, answer);
 		}
 	}
 	
 	private boolean correctChannel(ReceivePrivmsg event) {
 		String currChannel = event.getChannel();
-		if (COUNTDOWN_CHANNEL.equals(currChannel)) {
+		if (COUNTDOWN_CHANNEL.equals(currChannel)) {			
+			return true;
+		} else {
+			event.reply("To play Countdown join : " + COUNTDOWN_CHANNEL);
+			return false;
+		}
+	}
+	  
+   private boolean isPrivate(ReceivePrivmsg event) {
+       try {
+           bot.getChannel(event.getChannel()).getNicks();
+       }
+       catch (Exception e) {
+           return true;
+       }
+       return false;
+   }
+   
+   private boolean inGameChannel(ReceivePrivmsg event) {
+	   String currChannel = event.getChannel();
+	   if (COUNTDOWN_CHANNEL.equals(currChannel)) {
+	       try {
+	          return bot.getChannel(event.getChannel()).getNicks().contains(event.getSender());
+	       }
+	       catch (Exception e) {
+	           return false;
+	       }
+	   }
+       return true;
+   }
+	
+	private boolean correctSolutionChannel(ReceivePrivmsg event) {
+		String currChannel = event.getChannel();
+		if (COUNTDOWN_CHANNEL.equals(currChannel) || (isPrivate(event) && inGameChannel(event))) {		
 			return true;
 		} else {
 			event.reply("To play Countdown join : " + COUNTDOWN_CHANNEL);
@@ -168,7 +201,6 @@ public class Countdown {
 	private synchronized void startCountdownGame(ReceivePrivmsg event) {
 		if (currGame == null) {
 			currGame = new CountdownGame();
-			currGame.interpretedAnswer("hello");
 			event.reply(currGame.getCountdownIntro());
 			startTimer(event);
 		} else {
@@ -176,21 +208,27 @@ public class Countdown {
 		}
 	}
 	
-	private synchronized void checkAnswer(ReceivePrivmsg event, String answer) {
-		System.out.println("checking answer");
+	private synchronized void checkAnswer(ReceivePrivmsg event, String submission) {
 		String contestant = BaseNick.toBaseNick(event.getSender());
 		if (currGame != null) {
-			event.reply("current game is ongoing");
-			if (currGame.isCorrect(answer)) {
-				stopTimer();	
-                event.reply(String.format("Congrats %s, you win %d karma!", contestant, currGame.getStakes()));
-                KarmaDB.modNounKarma(contestant, currGame.getStakes());
-                currGame = null;
-			} else {
-                event.reply(String.format("Sorry %s! That is incorrect, you lose %d karma.",
+			try {
+				int contestantAnswer = currGame.interpretedAnswer(submission);
+				if (!currGame.submissions.containsKey(contestant)) {
+					currGame.submissions.put(contestant, submission+" = "+contestantAnswer);
+					if (isPrivate(event)) {
+						event.reply(String.format("Your submission of %s has been recieved!", submission));
+					} else {
+						event.reply(String.format("%s's submission recieved!", contestant));
+					}
+				} else {
+		            event.reply(String.format("Sorry %s, you've already submitted for this game.", contestant));
+				}			
+			} catch (Exception e) {
+				e.printStackTrace();
+                event.reply(String.format("Sorry %s! You have put an invalid answer, you lose %d karma.",
                 		contestant, currGame.getStakes()));
                 KarmaDB.modNounKarma(contestant, -1 * currGame.getStakes());
-                }
+			}
 		} else {
 			event.reply("No active game. Start a new one with !countdown");
 		}
@@ -212,8 +250,17 @@ public class Countdown {
     }
    
    private void timeoutTimer(ReceivePrivmsg event) {
-       String message = currGame.getTimeoutString();
-       event.reply(message);
+	   Set<String> keys = currGame.submissions.keySet();
+	   event.reply(String.format("Your time is up! The target number was %s \n",currGame.getTargetNumber()));
+	   if (!keys.isEmpty()) {
+		   event.reply("The final submissions are: \n");
+		   for (String key : keys) {
+			   event.reply(key+", with "+ currGame.submissions.get(key)+"\n");
+		   }
+		   
+	   } else {
+		   event.reply("There were no submissions for this game. Better luck next time!");
+	   }
        currGame = null;
    }
 
@@ -221,6 +268,4 @@ public class Countdown {
        timer.shutdownNow();
        timer = Executors.newFixedThreadPool(1);
    }
-	
-	
 }
