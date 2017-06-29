371682ed2ad8b5938b80435e239c0bac29184b46
Christopher Koehler <chkoehle@redhat.com>
2017-06-29 12:26:45 -0400

    Added capitalization and unit tests

---
+++ vilebot/src/test/java/vilebot/util/StringUtilTest.java
@@ -0,0 +1,30 @@
+package test.java.vilebot.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.java.com.oldterns.vilebot.util.StringUtil;
+
+public class StringUtilTest {
+
+    @Test
+    public void nullArgReturnsNull() {
+        assertEquals(null, StringUtil.capitalizeFirstLetter(null));
+    }
+    
+    @Test
+    public void emptyStringReturnsEtmpy() {
+        assertEquals("", StringUtil.capitalizeFirstLetter(""));
+    }
+    
+    @Test
+    public void convertsToCapitalizationProperly() {
+        assertEquals("\0", StringUtil.capitalizeFirstLetter("\0"));
+        assertEquals("Heh", StringUtil.capitalizeFirstLetter("heh"));
+        assertEquals("123 test", StringUtil.capitalizeFirstLetter("123 test"));
+        assertEquals("Some Fancy sentence. HERE", StringUtil.capitalizeFirstLetter("some Fancy sentence. HERE"));
+        assertEquals("_", StringUtil.capitalizeFirstLetter("_"));
+        assertEquals("YES NO MAYBE", StringUtil.capitalizeFirstLetter("YES NO MAYBE"));
+    }
+}
