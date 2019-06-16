d7e62fb0ddd2fced0cc3a3caefefe9ce2c1eecbd
Julian Cui <jucui@redhat.com>
2019-06-16 12:22:44 -0400

    Add headline and link parsing functionality

--- vilebot/src/main/java/com/oldterns/vilebot/handlers/user/News.java
+++ vilebot/src/main/java/com/oldterns/vilebot/handlers/user/News.java
@@ -7,9 +7,15 @@
 
 package com.oldterns.vilebot.handlers.user;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
+import java.net.URL;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,13 +34,32 @@ public class News
 
         if ( match.matches() )
         {
-            printHeadlines( event );
+            try
+            {
+                printHeadlines( event );
+            }
+            catch ( Exception e )
+            {
+                event.respondWith( "Unable to retrieve headlines.");
+                e.printStackTrace();
+            }
         }
     }
 
     private void printHeadlines( GenericMessageEvent event )
+        throws Exception
     {
-        event.respondWith("Get headlines");
+        URL feedSource = new URL( CBC_URL );
+
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build( new XmlReader( feedSource ) );
+
+        List<SyndEntry> entries = feed.getEntries();
+
+        for ( int i = 0; i < 5; i++ )
+        {
+            event.respondWith( entries.get( i ).getTitle() + " -> " + entries.get( i ).getLink() );
+        }
     }
 
 }
