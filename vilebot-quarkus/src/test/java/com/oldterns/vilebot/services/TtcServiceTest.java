97085aa5e42429449e11adc1529b723c9680fa3f
Christopher Chianelli <chris.chianelli@gmail.com>
2021-05-30 23:30:08 -0400

    Add tests for TtcService in vilebot-quarkus

---
+++ vilebot-quarkus/src/test/java/com/oldterns/vilebot/services/TtcServiceTest.java
@@ -0,0 +1,31 @@
+package com.oldterns.vilebot.services;
+
+import com.oldterns.vilebot.util.TestUrlStreamHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TtcServiceTest {
+
+    static TestUrlStreamHandler urlStreamHandler;
+
+    @BeforeAll
+    public static void setup() {
+        urlStreamHandler = TestUrlStreamHandler.setup();
+    }
+
+    @Test
+    public void testPrintAlerts() throws MalformedURLException {
+        TtcService ttcService = new TtcService();
+        urlStreamHandler.mockConnection(new URL(TtcService.TTC_URL),
+                TtcServiceTest.class.getResourceAsStream("ttc-example-alerts.html"));
+        assertThat(ttcService.printAlerts()).isEqualTo(
+                "Line 1: There is no subway service between Sheppard-Yonge and Bloor-Yonge stations due to ATC signal system testing. Shuttle buses are running.\n"
+                + "Line 1: Next Mon. through Thurs. nightly starting at midnight, customers will board northbound trains on southbound platforms at Pioneer Village and Highway 407 stations during single-track operation. Expect slightly longer wait times.\n"
+                + "Line 1: Mon. through Thurs., subway service between Finch and Eglinton stations will end nightly at 11pm for tunnel remediation. Shuttle buses will run.");
+    }
+}
